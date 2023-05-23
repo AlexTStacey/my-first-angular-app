82b9a4eccf2a34635f16cf8fb3d3e80a6ea12a87
AlexTStacey <alex.stacey@roweit.co.uk>
2023-05-23 10:16:37 +0100

    update quotes service to use aws sdk

--- src/app/quotesService.ts
+++ src/app/quotesService.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import * as AWS from 'aws-sdk';
   
 @Injectable({
   providedIn: 'root'
 })
 export class GetQuotesService {
 
-  private url = "";
+  private url = this.fetchApitUrlFromStack();
     
   constructor(private httpClient: HttpClient) { };
   
@@ -24,6 +25,35 @@ export class GetQuotesService {
 
   deleteQuote(id: any, data: any){
     return this.httpClient.delete(this.url + "/" + id, data);
+  }
+
+  fetchApitUrlFromStack(){
+    const  cloudformation = new AWS.CloudFormation();
+    let quotesApiUrl = "";
+
+    cloudformation.describeStacks({StackName: 'CdkStack'}, (err, data) => {
+      if (err){
+        console.error("Failed to retrive stack outputs - " + err);
+        quotesApiUrl = "";
+      } else {
+        //aws cloudformation --region eu-west-2 describe-stacks --stack-name CdkStack --query "Stacks[0].Outputs[?OutputKey=='quotesApiUrl']" --output text
+        const stacksList = data?.Stacks;
+        if(stacksList){
+          for(let i = 0; stacksList?.length < i; i++){
+            let apiUrl = stacksList[i].Outputs?.find(o => o.ExportName === 'quotesApiUrl');
+            if(apiUrl && apiUrl.OutputValue){
+              console.log(apiUrl.OutputKey);
+              quotesApiUrl = apiUrl.OutputValue;
+            }
+          }
+        } else {
+          console.error("Failed to retrive stack outputs");
+          quotesApiUrl = "";
+        }
+      }
+    });
+
+    return quotesApiUrl;
   }
   
 }
